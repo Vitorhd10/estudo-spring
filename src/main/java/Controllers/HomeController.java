ffd7c29fd07ecd13a51ccfa00da81fc2e46e6c00
vitor hugo <vitorhd10@gmail.com>
2021-07-26 16:49:57 -0300

    commit

---
+++ src/main/java/Controllers/HomeController.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return"login";
+    }
+
+    @RequestMapping("/")
+    public String index (){
+        return "index";
+    }
+
+}
